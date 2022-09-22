5d3d16cdfa8396659c8248116cd4494008fb30e4
SeungGu Kang <alex9366@naver.com>
2022-09-22 16:19:54 +0900

    1주차 실습 과제

---
+++ Ch06/app/src/main/java/com/sgkang/ch06/SubActivity.kt
@@ -0,0 +1,11 @@
+package com.sgkang.ch06
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SubActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sub)
+    }
+}
