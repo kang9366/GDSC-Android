72e2d934b7269294437cfd8eaf711babcc710c2b
SeungGu Kang <alex9366@naver.com>
2022-11-24 04:28:27 +0900

    9주차 실습과제

---
+++ ch10/src/main/java/com/sgkang/ch15/Receiver.kt
@@ -0,0 +1,17 @@
+package com.sgkang.ch15
+
+import android.app.RemoteInput
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.util.Log
+import android.widget.Toast
+
+class Receiver: BroadcastReceiver() {
+    override fun onReceive(p0: Context?, p1: Intent?) {
+        val KEY_TEXT_REPLY = "key_text_reply"
+        val remoteInput = RemoteInput.getResultsFromIntent(p1)
+        val replyMessage = remoteInput.getCharSequence(KEY_TEXT_REPLY).toString()
+        Toast.makeText(p0, replyMessage, Toast.LENGTH_LONG).show()
+    }
+}
