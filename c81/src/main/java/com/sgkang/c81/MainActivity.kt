da7c2b8edfcf20279363a584e37e2a122a0d41f0
SeungGu Kang <alex9366@naver.com>
2022-11-28 13:34:21 +0900

    LocationManager

---
+++ c81/src/main/java/com/sgkang/c81/MainActivity.kt
@@ -0,0 +1,52 @@
+package com.sgkang.c81
+
+import android.Manifest
+import android.content.pm.PackageManager
+import android.location.LocationManager
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
+
+class MainActivity : AppCompatActivity() {
+    lateinit var resultView: TextView
+    lateinit var manager: LocationManager
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        resultView = findViewById<TextView>(R.id.resultView)
+        manager = getSystemService(LOCATION_SERVICE) as LocationManager
+
+        val launcher = registerForActivityResult(
+            ActivityResultContracts.RequestPermission()
+        ){ isGranted ->
+            if(isGranted){
+                getLocation()
+            }else{
+                Toast.makeText(this, "denied..", Toast.LENGTH_LONG).show()
+            }
+        }
+
+        val status = ContextCompat.checkSelfPermission(this,
+        "android.permission.ACCESS_FINE_LOCATION")
+        if(status == PackageManager.PERMISSION_GRANTED){
+            getLocation()
+        }else{
+            launcher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+        }
+    }
+    fun getLocation(){
+        val location = manager.getLastKnownLocation(LocationManager.GPS_PROVIDER)
+        location?.let{
+            val latitude = location.latitude
+            val longitude = location.longitude
+            val accuracy = location.accuracy
+            val time = location.time
+
+            resultView.text = "$latitude, $longitude, $accuracy, $time"
+        }
+    }
+}
