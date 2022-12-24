947f885809d9858f606d4db1fcd99358137d7826
SeungGu Kang <alex9366@naver.com>
2022-12-24 10:28:44 +0900

    RecyclerView

---
+++ c92/src/main/java/com/sgkang/c92/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.sgkang.c92
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.sgkang.c92.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val datas = mutableListOf<String>()
+        for(i in 1..10) {
+            datas.add("item $i")
+        }
+
+        binding.recyclerView.layoutManager = LinearLayoutManager(this)
+        binding.recyclerView.adapter = MyAdapter(datas)
+        binding.recyclerView.addItemDecoration(DividerItemDecoration(this, LinearLayoutManager.VERTICAL))
+    }
+}
